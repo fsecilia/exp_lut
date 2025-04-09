b3536071de9aa5e659e0ccc6567d6befa97f40de
Frank Secilia <frank.secilia@gmail.com>
2025-04-08 22:47:18 -0400

    magnitude is not unique enough to warrant the complexity
    The curves produced by this extra term are not different enough from
    curves that can already be created.
    
    In the end, this term mostly just serves to divide out below c more
    strongly, but in a way that is slightly different because of the -1s.
    The difference from just subtracting mag from nonlinearity doesn't
    seem important enough to keep it in.

--- exp_lut.py
+++ exp_lut.py
@@ -3,14 +3,14 @@
 import math
 import argparse
 
-default_crossover = 8.3
-default_nonlinearity = 3.8
-default_magnitude = -1.7957674906255938191612700193035
-default_sensitivity = 3.3814424928542726157295532724884
-default_limit = 5.5
-default_limit_rate = 1.0
-
 table_size = 50
+
+default_crossover = 8.3
+default_nonlinearity = 0.7
+default_magnitude = 0
+default_sensitivity = 3.38
+default_limit = 4
+default_limit_rate = 1.0
 
 # exponential curve:
 # - shifted to go through (0, 0)
