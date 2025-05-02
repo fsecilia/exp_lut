18995e35c16cb3f1844f772abaa82a2544da0c73
Frank Secilia <frank.secilia@gmail.com>
2025-05-02 14:53:42 -0400

    floored log for glass and superlight

--- exp_lut.py
+++ exp_lut.py
@@ -73,11 +73,11 @@ class params_t:
 
 default_params = params_t(
     curve = "floored_log",
-    floor = 0.011,
+    floor = 0.0,
     limit = 0.0,
     limit_rate = 0.0,
     sensitivity = 50,
-    crossover = 25,
+    crossover = 35,
     nonlinearity = 1.0,
     magnitude = 0.5,
 )
